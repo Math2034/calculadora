6fe5047a441e32ca07ee300c174f3c40d66d8150
Matheus Fonseca <61693870+Math2034@users.noreply.github.com>
2025-02-12 10:03:33 +0800

    Update task1.js

--- js/task1.js
+++ js/task1.js
@@ -4,19 +4,38 @@
 Calculate the square and cube of the number. A number squared is a number that is multiplied by itself; e.g., 22 is equal to 4 (2 * 2). A number cubed is a number that is multiplied by itself twice e.g., 23 is equal to 8 (2 * 2 * 2). 
 calculates the number's factorial. A factorial is the product of an integer and all the integers below it; e.g., the factorial of 4 is equal to 24 (4 * 3 * 2 * 1).
 */
+
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+function cubed(n){
+  return n * n * n
+}
+
+function squared(n){
+  return n*n
+}
+
 function calculate(){
 
-  var number = document.getElementById('number').value;
-  var factorial = document.getElementById('factorial').value;
-  var squared = document.getElementById('squared').value;
-  var cubed = document.getElementById('cubed').value;
+  const numberInput = document.getElementById("number");
+  const number = parseInt(numberInput.value);
 
-  // factorial
-  squared = Math.pow(number, 2)
-  console.log(squared);
+  document.getElementById('factorial').value = factorial(number)
+  document.getElementById('squared').value = squared(number)
+  document.getElementById('cubed').value = cubed(number)
+
 	
 // End of calculate() function.
 }
+
+document.getElementById("calculate").addEventListener("click", calculate);
 
 function init(){
 
